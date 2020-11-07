5d95272a5d09312b9fca993a71884d0a2fdfe9a2
rocaetani <rocaetani@gmail.com>
2020-11-06 21:21:33 -0300

    Second Commit
    teste

---
+++ Assets/Scripts/Aula3/GoToRandomPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GoToRandomPosition : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if (Vector3.Distance(transform.position, target.position) < 0.00001f)
+        {
+            Vector3 new_position = target.position;
+            new_position.x = Random.Range(-4.5f, 4.5f);
+            new_position.z = Random.Range(-4.5f, 4.5f);
+            target.position = new_position;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Aula3/RandomPositionW_Plane.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class RandomPositionW_Plane : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    public Transform plane;
+    private Mesh mesh;
+    private Vector3 meshMin;
+    private Vector3 meshMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+        mesh = plane.GetComponent<MeshFilter>().mesh;
+        meshMin = mesh.bounds.min;
+        meshMax = mesh.bounds.max;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if (Vector3.Distance(transform.position, target.position) < 0.00001f)
+        {
+            Vector3 newPosition = target.position;
+            newPosition.x = Random.Range(meshMin.x, meshMax.x);
+            newPosition.z = Random.Range(meshMin.z, meshMax.z);
+            target.position = newPosition;
+        }
+
+    }
+}
